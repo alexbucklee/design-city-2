b4b8b2906c13fcfe13888d7dbcbed7fee4fbafc0
lyostya <lyostya97@yandex.ru>
2021-03-30 12:17:10 +0300

    upd

--- source/blocks/page.js
+++ source/blocks/page.js
@@ -26,3 +26,15 @@ for (let anchor of anchors) (
         })
     })
 );
+
+function showmap() {
+    let process = document.querySelector('.process');
+    let processTop = process.offsetTop;
+
+    if (window.pageYOffset > processTop) {
+        map.insertAdjacentHTML('afterbegin', '<iframe tabindex="-1" src="https://yandex.ru/map-widget/v1/?um=constructor%3A19de43c8666d69edc9d0aac65d6716b49b455802513d90c1a3251165ed2170d9&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>');
+        document.removeEventListener('scroll', showmap);
+    }
+}
+
+document.addEventListener('scroll', showmap);
